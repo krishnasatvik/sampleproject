a88b42daa1e4be419840cf00e838c7104284a039
krishnasatvik <109426680+krishnasatvik@users.noreply.github.com>
2023-06-21 11:42:19 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "credentials_file" {
+  type    = string
+  default = "krishtest-390320-55a2c26ed699.json"
+}
+
+variable "project_id" {
+  type    = string
+  default = "krishtest-390320"
+}
