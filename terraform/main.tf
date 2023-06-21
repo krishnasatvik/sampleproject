3b7e9cf7e8ca6b9445b95916975fc8ec32142d9e
krishnasatvik <109426680+krishnasatvik@users.noreply.github.com>
2023-06-21 11:33:10 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,11 @@
+provider "google" {
+  credentials = file("var.credentials_file")
+  project     = "var.project_id"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name     = "my-test-bucket"
+  location = "us-central1"
+}
 
