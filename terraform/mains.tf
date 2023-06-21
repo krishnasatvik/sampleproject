2fcbbe3878071f3970beca89ac222721708dc887
krishnasatvik <109426680+krishnasatvik@users.noreply.github.com>
2023-06-21 11:32:45 -0400

    Create mains.tf

---
+++ terraform/mains.tf
@@ -0,0 +1,10 @@
+provider "google" {
+  credentials = file("var.credentials_file")
+  project     = "var.project_id"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name     = "my-test-bucket"
+  location = "us-central1"
+}
